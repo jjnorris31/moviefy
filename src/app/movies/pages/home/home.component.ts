50c39b1011a7ea51adeae0c5125bfc2d6a9c4660
jjnorris31 <jjnorris31@gmail.com>
2022-03-01 16:43:36 -0600

    feat: added username to navbar in home component

--- src/app/movies/pages/home/home.component.ts
+++ src/app/movies/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../../../services/auth.service";
 
 @Component({
   selector: 'app-home',
@@ -7,10 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  public username: String | undefined;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.username = this.authService.user.username;
   }
 
 }
